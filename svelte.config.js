5b1e5ef1b01f3b166b4dc1dc8b01ff7501f24944
MaratMedvedev <freiden2004@gmail.com>
2022-07-06 16:26:24 +0300

    Work with actions

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,17 @@ import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
+const dev = process.env.NODE_ENV === 'development';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/SvelteKitProject',
+		}
 	}
 };
 
